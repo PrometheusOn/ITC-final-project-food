d2e0dad55a12f881caabb412c100a9494bff0de1
PrometheusOn <ramazanfajzov@mail.ru>
2023-05-13 18:10:40 +0500

    .

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    
+    <div>{{ state.count }}</div>
+    <button @click="increment">
+      click
+    </button>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref } from 'vue'
 
+const state = ref({ count: 0 })
+
+function increment() {
+  state.value.count++
+}
 </script>
 
 
 <style lang="scss" scoped>
-  
+
 </style>
